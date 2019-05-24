b0215ea9074c86008f26ee0ebe68476e0c7fead3
kulia26 <kulinich261@gmail.com>
2019-05-25 01:17:03 +0300

    add package-lock.json to .gitignore

--- server.js
+++ server.js
@@ -1,40 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path')
+const express = require('express');
 
 const hostname = '0.0.0.0';
 const port = 8080;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  let filePath = path.join(__dirname, path.sep,'app');
-  filePath += req.url !== '/' ? req.url : "/index.html";
-
-
-  fs.exists(filePath, (exists) => {
-
-    if (exists) {
-      let types = {
-        '.css':'text/css',
-        '.html':'text/html',
-        '.js':'text/javascript',
-        '.pdf':'application/pdf',
-      }
-
-      let type = Object.keys(types).includes(path.extname(filePath)) ? types[path.extname(filePath)] : 'text/plain';
-      res.setHeader('Content-Type', type);
-
-      res.statusCode = 200;
-      let readStream = fs.createReadStream(filePath);
-      readStream.pipe(res);
-    } else {
-      res.setHeader("Content-Type", "text/plain");
-      res.statusCode = 404;
-      res.end("Page not found");
-    };
-  });
-
+app.get(/(\/index\.html)|(\/)/,(req, res)=>{
+  req.sendFile(res.params.file);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
