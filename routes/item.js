e872e79ada5820e12be46f022cf50cb565ff9acc
kulia26 <kulinich261@gmail.com>
2019-05-27 20:24:06 +0300

    item pug

--- routes/item.js
+++ routes/item.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('item', { title: 'Про нас | Каталог', item : 'MacBook Pro mid 2013'});
+  res.render('item', { title: 'MacBook Pro mid 2013'+' | Каталог', item : 'MacBook Pro mid 2013'});
 });
 
 module.exports = router;
