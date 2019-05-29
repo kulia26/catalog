eff6760715496a059d9e9578ed30e5e4ac1debf7
kulia26 <kulinich261@gmail.com>
2019-05-29 22:27:17 +0300

    init file

---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const multer  = require('multer');
+const path = require('path');
+const crypto = require('crypto');
+const fs = require('fs');
+
+
+router.get('/', (req, res) => {
+  res.render('contacts', { title: 'Зворотній зв\'язок | Каталог' });
+});
+
+
+module.exports = router;
